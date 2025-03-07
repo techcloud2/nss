87c21d2c115f67af485737a05324138c3fbaa54d
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 23:08:50 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,24 +1,3 @@
-variable "client_id" {
-  description = "The client ID of the Azure service principal"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "The client secret of the Azure service principal"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "The tenant ID of the Azure subscription"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "The subscription ID of the Azure subscription"
-  type        = string
-}
-
-# Define multiple VM configurations
 variable "vm_configs" {
   description = "List of VM configurations"
   type = list(object({
@@ -27,17 +6,36 @@ variable "vm_configs" {
     os_version           = string
     location             = string
     resource_group_name  = string
+    create_rg           = bool
     vnet_name            = string
-    create_vnet          = bool
+    create_vnet         = bool
     vnet_address_space   = string
     subnet_name          = string
-    create_subnet        = bool
+    create_subnet       = bool
     subnet_address_prefix = string
-    create_rg            = bool
     vm_size              = string
     admin_username       = string
     admin_password       = string
     os_disk_type         = string
     os_disk_size         = number
+
+    security_rules = list(object({
+      name                       = string
+      priority                   = number
+      direction                  = string
+      access                     = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    }))
+
+    os_image = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
   }))
 }
