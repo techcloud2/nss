67f76e6232f0fe2d0f6120321c18b665bbdeeb18
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 17:34:56 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,8 +3,8 @@
 output "vm_details" {
   value = { for vm in var.vm_configs : vm.vm_name => {
     public_ip = try(
-      azurerm_linux_virtual_machine.linux_vm[vm.vm_name].public_ip_address, 
-      azurerm_windows_virtual_machine.windows_vm[vm.vm_name].public_ip_address, 
+      azurerm_linux_virtual_machine.linux_vm[vm.vm_name].public_ip_address,
+      azurerm_windows_virtual_machine.windows_vm[vm.vm_name].public_ip_address,
       "N/A"
     )
     username  = vm.admin_username
