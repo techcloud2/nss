687c6c469fa4ccf376c0a8c024bbdb86a7047a07
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 18:09:33 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,18 +1,4 @@
-### `output.tf`
-
-output "vm_details" {
-  value = { for vm in var.vm_configs : vm.vm_name => {
-    public_ip = try(
-      azurerm_linux_virtual_machine.linux_vm[vm.vm_name].public_ip_address,
-      azurerm_windows_virtual_machine.windows_vm[vm.vm_name].public_ip_address,
-      "N/A"
-    )
-    username  = vm.admin_username
-    password  = random_password.password[vm.vm_name].result
-    location  = vm.location
-    resource_group = vm.resource_group_name
-    os_type   = vm.os_type
-    os_version = vm.os_version
-  }}
-  sensitive = true
+### `outputs.tf`
+output "vm_public_ip" {
+  value = { for idx, vm in azurerm_linux_virtual_machine.vm : idx => vm.public_ip_address }
 }
