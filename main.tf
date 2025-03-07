2ee7f4ad9c543f1ca5a37e0aacb6c8966fc2f308
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 23:45:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Resource Group Creation
 resource "azurerm_resource_group" "rg" {
   for_each = { for vm in var.vm_configs : vm.resource_group_name => vm if vm.create_rg }
 
@@ -9,47 +10,45 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-# Conditionally create a new Virtual Network or fetch an existing one
+# Virtual Network Creation
 resource "azurerm_virtual_network" "new_vnet" {
   for_each = { for vm in var.vm_configs : vm.vnet_name => vm if vm.create_vnet }
 
   name                = each.value.vnet_name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
-  address_space       = [each.value.vnet_address_space]
-
-  lifecycle {
-    prevent_destroy = true
-  }
+  address_space       = [each.value.vnet_address_space] # Ensure correct format
 }
 
+# Fetch existing Virtual Network
 data "azurerm_virtual_network" "existing_vnet" {
   for_each = { for vm in var.vm_configs : vm.vnet_name => vm if !vm.create_vnet }
 
   name                = each.value.vnet_name
   resource_group_name = each.value.resource_group_name
-  depends_on          = [azurerm_virtual_network.new_vnet]
 }
 
-# Conditionally create a new Subnet or fetch an existing one
+# Subnet Creation
 resource "azurerm_subnet" "new_subnet" {
   for_each = { for vm in var.vm_configs : vm.subnet_name => vm if vm.create_subnet }
 
   name                 = each.value.subnet_name
   resource_group_name  = each.value.resource_group_name
   virtual_network_name = azurerm_virtual_network.new_vnet[each.value.vnet_name].name
-  address_prefixes     = [each.value.subnet_address_prefix] # FIXED: Correct attribute name
+  address_prefixes     = [each.value.subnet_address_prefix] # Ensure correct format
 }
 
+# Fetch existing subnet
 data "azurerm_subnet" "existing_subnet" {
   for_each = { for vm in var.vm_configs : vm.subnet_name => vm if !vm.create_subnet }
 
   name                 = each.value.subnet_name
   virtual_network_name = data.azurerm_virtual_network.existing_vnet[each.value.vnet_name].name
   resource_group_name  = each.value.resource_group_name
-  depends_on           = [azurerm_virtual_network.new_vnet] # FIXED: Ensure the VNet is available before fetching the subnet
+  depends_on           = [azurerm_virtual_network.new_vnet] # Ensure VNet exists before reading
 }
 
+# Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   for_each = { for vm in var.vm_configs : vm.vm_name => vm }
 
@@ -73,6 +72,7 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+# Network Interface
 resource "azurerm_network_interface" "nic" {
   for_each = { for vm in var.vm_configs : vm.vm_name => vm }
 
@@ -90,6 +90,7 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+# Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   for_each = { for vm in var.vm_configs : vm.vm_name => vm if vm.os_type == "Linux" }
 
@@ -99,8 +100,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.nic[each.value.vm_name].id]
   size                  = each.value.vm_size
 
-  admin_username = each.value.admin_username
-  admin_password = each.value.admin_password
+  admin_username                  = each.value.admin_username
+  admin_password                  = each.value.admin_password
   disable_password_authentication = false
 
   os_disk {
@@ -121,6 +122,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
+# Windows Virtual Machine
 resource "azurerm_windows_virtual_machine" "vm" {
   for_each = { for vm in var.vm_configs : vm.vm_name => vm if vm.os_type == "Windows" }
 
@@ -149,29 +151,4 @@ resource "azurerm_windows_virtual_machine" "vm" {
   lifecycle {
     prevent_destroy = true
   }
-}
-
-# Managed Data Disks
-resource "azurerm_managed_disk" "data_disk" {
-  for_each = { for vm in var.vm_configs : vm.vm_name => vm if lookup(vm, "data_disks", []) != [] }
-
-  name                 = "${each.value.vm_name}-datadisk"
-  location             = each.value.location
-  resource_group_name  = each.value.resource_group_name
-  storage_account_type = each.value.os_disk_type
-  create_option        = "Empty"
-  disk_size_gb         = each.value.os_disk_size
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
-  for_each = { for vm in var.vm_configs : vm.vm_name => vm if lookup(vm, "data_disks", []) != [] }
-
-  managed_disk_id    = azurerm_managed_disk.data_disk[each.value.vm_name].id
-  virtual_machine_id = lookup(merge(
-    { for vm in azurerm_linux_virtual_machine.vm : vm.name => vm.id },
-    { for vm in azurerm_windows_virtual_machine.vm : vm.name => vm.id }
-  ), each.value.vm_name, null)
-
-  lun     = 0
-  caching = "ReadWrite"
 }
