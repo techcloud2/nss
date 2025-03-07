ba6c2947919234aef19952eb2fdc2925161d38f9
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 18:12:42 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,5 +1,4 @@
-### `var.tf`
-
+### var.tf
 variable "client_id" {
   description = "The client ID of the Azure service principal"
   type        = string
@@ -22,21 +21,21 @@ variable "subscription_id" {
 
 variable "vm_configs" {
   type = list(object({
-    vm_name              = string
-    os_type              = string
-    os_version           = string
-    location             = string
-    resource_group_name  = string
-    vnet_name            = string
-    create_vnet          = optional(bool, false)
-    vnet_address_space   = string
-    subnet_name          = string
-    create_subnet        = optional(bool, false)
+    vm_name               = string
+    os_type               = string
+    os_version            = string
+    location              = string
+    resource_group_name   = string
+    vnet_name             = string
+    create_vnet           = optional(bool, false)
+    vnet_address_space    = string
+    subnet_name           = string
+    create_subnet         = optional(bool, false)
     subnet_address_prefix = string
-    create_rg            = optional(bool, false)
-    vm_size              = string
-    admin_username       = string
-    os_disk_type         = string
-    os_disk_size         = number
+    create_rg             = optional(bool, false)
+    vm_size               = string
+    admin_username        = string
+    os_disk_type          = string
+    os_disk_size          = number
   }))
 }
