103d1d80b9ea4638d86c1985ee2dc0f2c51cae36
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 17:36:05 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,4 @@
+# Azure Authentication Variables
 variable "client_id" {
   description = "The client ID of the Azure service principal"
   type        = string
@@ -20,16 +21,45 @@ variable "subscription_id" {
 
 variable "vm_configs" {
   type = list(object({
-    vm_name             = string
-    location            = string
-    resource_group_name = string
-    vnet_name           = string
-    subnet_name         = string
-    vm_size             = string
-    admin_username      = string
-    os_disk_size        = number
-    os_disk_type        = string
-    os_type             = string
-    os_version          = string
+    vm_name              = string
+    os_type              = string
+    os_version           = string
+    location             = string
+    resource_group_name  = string
+    vnet_name            = string
+    subnet_name          = string
+    vm_size              = string
+    admin_username       = string
+    os_disk_type         = string
+    os_disk_size         = number
   }))
+
+  default = [
+    {
+      vm_name             = "linux-vm-1"
+      os_type             = "linux"
+      os_version          = "20_04-lts-gen2"
+      location            = "East US"
+      resource_group_name = "rg-linux"
+      vnet_name           = "vnet-linux"
+      subnet_name         = "subnet-linux"
+      vm_size             = "Standard_D2s_v3"
+      admin_username      = "azureuser"
+      os_disk_type        = "Standard_LRS"
+      os_disk_size        = 30
+    },
+    {
+      vm_name             = "windows-vm-1"
+      os_type             = "windows"
+      os_version          = "2019-Datacenter-smalldisk"
+      location            = "East US"
+      resource_group_name = "rg-windows"
+      vnet_name           = "vnet-windows"
+      subnet_name         = "subnet-windows"
+      vm_size             = "Standard_D4s_v3"
+      admin_username      = "adminuser"
+      os_disk_type        = "StandardSSD_LRS"
+      os_disk_size        = 127
+    }
+  ]
 }
