730f7a17903171ed52a53a775c51d9431ac5ef7e
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 17:47:17 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -28,8 +28,10 @@ variable "vm_configs" {
     resource_group_name  = string
     vnet_name            = string
     create_vnet          = bool
+    vnet_address_space   = string
     subnet_name          = string
     create_subnet        = bool
+    subnet_address_prefix = string
     vm_size              = string
     admin_username       = string
     os_disk_type         = string
@@ -38,34 +40,38 @@ variable "vm_configs" {
 
   default = [
     {
-      vm_name             = "linux-vm-1"
-      os_type             = "linux"
-      os_version          = "20_04-lts-gen2"
-      location            = "Central India"
-      resource_group_name = "rg-x"
-      vnet_name           = "vnet-linux"
-      create_vnet         = true
-      subnet_name         = "subnet-linux"
-      create_subnet       = true
-      vm_size             = "Standard_B1s"
-      admin_username      = "azureuser"
-      os_disk_type        = "Standard_LRS"
-      os_disk_size        = 30
+      vm_name              = "linux-vm-1"
+      os_type              = "linux"
+      os_version           = "20_04-lts-gen2"
+      location             = "Central India"
+      resource_group_name  = "rg-x"
+      vnet_name            = "vnet-linux"
+      create_vnet          = true
+      vnet_address_space   = "10.0.0.0/16"
+      subnet_name          = "subnet-linux"
+      create_subnet        = true
+      subnet_address_prefix = "10.0.1.0/24"
+      vm_size              = "Standard_B1s"
+      admin_username       = "azureuser"
+      os_disk_type         = "Standard_LRS"
+      os_disk_size         = 32
     },
     {
-      vm_name             = "windows-vm-1"
-      os_type             = "windows"
-      os_version          = "2019-Datacenter-smalldisk"
-      location            = "Central India"
-      resource_group_name = "rg-x"
-      vnet_name           = "vnet-linux"
-      create_vnet         = true
-      subnet_name         = "subnet-windows"
-      create_subnet       = true
-      vm_size             = "Standard_B1s"
-      admin_username      = "adminuser"
-      os_disk_type        = "Standard_LRS"
-      os_disk_size        = 128
+      vm_name              = "windows-vm-1"
+      os_type              = "windows"
+      os_version           = "2019-Datacenter-smalldisk"
+      location             = "Central India"
+      resource_group_name  = "rg-x"
+      vnet_name            = "vnet-linux"
+      create_vnet          = false
+      vnet_address_space   = "10.0.0.0/16"
+      subnet_name          = "subnet-linux"
+      create_subnet        = false
+      subnet_address_prefix = "10.0.1.0/24"
+      vm_size              = "Standard_B1s"
+      admin_username       = "adminuser"
+      os_disk_type         = "Standard_LRS"
+      os_disk_size         = 128
     }
   ]
 }
