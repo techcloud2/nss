208c08ae3c17c47a86d2393bfdcbaee96cea5ecb
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 18:14:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-### `outputs.tf`
-output "vm_public_ip" {
-  value = { for idx, vm in azurerm_linux_virtual_machine.vm : idx => vm.public_ip_address }
+### outputs.tf
+output "vm_public_ips" {
+  value = { for vm in azurerm_linux_virtual_machine.vm : vm.name => vm.public_ip_address }
 }
