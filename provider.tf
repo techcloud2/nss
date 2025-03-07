dbd623535c03535b117edf5a0274a51794eca747
techcloud2 <136561832+techcloud2@users.noreply.github.com>
2025-03-07 18:33:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Ensures compatibility with Azurerm 3.x
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-
-  # Optional: Enables debug logging (useful for troubleshooting)
-  logging {
-    level = "info"
-  }
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
